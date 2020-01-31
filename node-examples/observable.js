0219b9a5ee4932d962eb72eea69074bdb229c2f7
suhanlee <suhan.lee.k@gmail.com>
2020-01-31 14:52:18 +0900

    add observable.js

---
+++ node-examples/observable.js
@@ -0,0 +1,13 @@
+const { Observable } = require('rxjs');
+
+const observable = new Observable(subscriber => {
+  subscriber.next(1);
+  subscriber.next(2);
+  subscriber.next(3);
+  setTimeout(() => {
+    subscriber.next(4);
+    subscriber.complete();
+  }, 1000);
+});
+
+observable.subscribe(console.log)
